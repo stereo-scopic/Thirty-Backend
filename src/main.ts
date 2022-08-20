ab4872ca8b4576eaff8efe2cdb7e558ebe723af0
shunnyjang <48276168+shunnyjang@users.noreply.github.com>
2022-08-20 17:35:28 +0900

    heroku deploy initial setting (#6)

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ async function bootstrap() {
   const appOptions = { cors: true };
   const app = await NestFactory.create(AppModule, appOptions);
   app.setGlobalPrefix('api/v1');
+  app.enableCors();
   await app.listen(3000);
 }
 
