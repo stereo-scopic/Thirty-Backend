5dbb46d922cb5510ad3e2047a9ad9685b9e0a1bf
shunnyjang <suuuu101@naver.com>
2022-09-12 02:04:20 +0900

    feat: get all prize and is user owned

---
+++ src/reward/reward.service.ts
@@ -0,0 +1,64 @@
+import { QueryOrder } from '@mikro-orm/core';
+import { InjectRepository } from '@mikro-orm/nestjs';
+import { EntityRepository } from '@mikro-orm/postgresql';
+import { Injectable } from '@nestjs/common';
+import { Prize, Reward, User } from 'src/entities';
+import { PrizeUserOwnedDto } from './dto/prize-user-owned.dto';
+
+@Injectable()
+export class RewardService {
+  constructor(
+    @InjectRepository(Reward)
+    private readonly rewardRepository: EntityRepository<Reward>,
+    @InjectRepository(Prize)
+    private readonly prizeRepository: EntityRepository<Prize>,
+  ) {}
+
+  async getUserRewardsList(user: User): Promise<any> {
+    const allPrizeList: Prize[] = await this.getAllPrizeList();
+    const rewardsListUserHave: Reward[] = await this.getRewardListUserHave(
+      user,
+    );
+    return this.getPrizeObjectWithUserOwned(allPrizeList, rewardsListUserHave);
+  }
+
+  private async getAllPrizeList() {
+    return this.prizeRepository.find({}, { orderBy: { id: QueryOrder.ASC } });
+  }
+
+  private async getRewardListUserHave(user: User) {
+    return this.rewardRepository.find({ user_id: user.id });
+  }
+
+  private async getPrizeObjectWithUserOwned(
+    prizes: Prize[],
+    rewards: Reward[],
+  ): Promise<any> {
+    const userRewardOwnedList: PrizeUserOwnedDto[] = [];
+    let rewardIdx = 0;
+
+    for (const _ of prizes) {
+      const {
+        created_at: prizeCreateDate,
+        updated_at: prizeUpdateDate,
+        ...prize
+      } = _;
+      const reward: Reward = rewards[rewardIdx];
+
+      const rewardData = {
+        created_at: null,
+        isOwned: false,
+      };
+      const prizeWithUserOwnedData: PrizeUserOwnedDto = {};
+      if (rewardIdx < rewards.length && prize.id === reward.prize_id) {
+        rewardData.created_at = reward.created_at;
+        rewardData.isOwned = true;
+        rewardIdx++;
+      }
+      Object.assign(prizeWithUserOwnedData, prize, rewardData);
+      userRewardOwnedList.push(prizeWithUserOwnedData);
+    }
+
+    return userRewardOwnedList;
+  }
+}
