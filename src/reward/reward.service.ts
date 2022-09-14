eea6a26c574f38775c0a08c7275d80c1563a719f
shunnyjang <suuuu101@naver.com>
2022-09-15 01:09:37 +0900

    feat: attendance rewards

--- src/reward/reward.service.ts
+++ src/reward/reward.service.ts
@@ -1,6 +1,6 @@
 import { QueryOrder } from '@mikro-orm/core';
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository } from '@mikro-orm/postgresql';
+import { EntityManager, EntityRepository } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
 import { Prize, Reward, User } from 'src/entities';
 import { PrizeUserOwnedDto } from './dto/prize-user-owned.dto';
@@ -12,6 +12,7 @@ export class RewardService {
     private readonly rewardRepository: EntityRepository<Reward>,
     @InjectRepository(Prize)
     private readonly prizeRepository: EntityRepository<Prize>,
+    private readonly em: EntityManager,
   ) {}
 
   async getUserRewardsList(user: User): Promise<PrizeUserOwnedDto[]> {
@@ -20,6 +21,28 @@ export class RewardService {
       user,
     );
     return this.getPrizeObjectWithUserOwned(allPrizeList, rewardsListUserHave);
+  }
+
+  /**
+   * 출석 리워드
+   * @param user
+   * @returns
+   */
+  async getRewardAttendance(user: User): Promise<object | void> {
+    const userId: string = user.id;
+    const fulfilledDays: string[] = ['01', '07', '30'];
+    for (const day of fulfilledDays) {
+      const prizeCode = 'AT' + day;
+
+      if (await this.isRewardExists(userId, prizeCode)) {
+        continue;
+      }
+      if (user.continuous_attendance !== Number(day)) {
+        continue;
+      }
+      await this.createReward(userId, prizeCode);
+      return this.getRewardResult(userId, prizeCode);
+    }
   }
 
   private async getAllPrizeList() {
@@ -31,6 +54,43 @@ export class RewardService {
 
   private async getRewardListUserHave(user: User) {
     return this.rewardRepository.find({ user_id: user.id });
+  }
+
+  private async createReward(userId: string, prizeCode: string): Promise<void> {
+    const reward: Reward = this.rewardRepository.create({
+      user_id: userId,
+      prize_code: prizeCode,
+    });
+    await this.rewardRepository.persistAndFlush(reward);
+  }
+
+  private async isRewardExists(userId: string, prizeCode: string) {
+    const reward: Reward = await this.rewardRepository.findOne({
+      user_id: userId,
+      prize_code: prizeCode,
+    });
+    if (!reward) {
+      return false;
+    }
+    return true;
+  }
+
+  private async getRewardResult(userId: string, prizeCode: string) {
+    const result = await this.em.execute(`
+    SELECT R.ID
+         , R.USER_ID
+         , R.CREATED_AT
+         , P.PRIZE_CODE
+         , P.NAME
+         , P.ILLUST
+      FROM REWARD R
+    INNER JOIN PRIZE P
+       ON P.PRIZE_CODE = R.PRIZE_CODE
+    WHERE 1=1
+      AND R.USER_ID = '${userId}'
+      AND P.PRIZE_CODE = '${prizeCode}'
+  `);
+    return result;
   }
 
   private async getPrizeObjectWithUserOwned(
