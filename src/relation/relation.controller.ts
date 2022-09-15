84dcb1125e34c5255486e70dad6e844836d96dbe
shunnyjang <suuuu101@naver.com>
2022-09-15 18:10:29 +0900

    feat: get relation list

--- src/relation/relation.controller.ts
+++ src/relation/relation.controller.ts
@@ -1,4 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { isArray } from 'class-validator';
+import { JwtAuthGuard } from 'src/auth/guards';
+import { Relation } from 'src/entities';
+import { RelationService } from './relation.service';
 
+@ApiTags('Relation')
+@ApiBearerAuth()
 @Controller('relation')
-export class RelationController {}
+@UseGuards(JwtAuthGuard)
+export class RelationController {
+    constructor(
+        private readonly relationService: RelationService
+    ) {}
+
+    @ApiOperation({ summary: `친구 목록` })
+    @ApiOkResponse({
+        type: Relation,
+        isArray: true
+    })
+    @Get('/')
+    async getRelationList(@Req() req): Promise<Relation[]> {
+        return this.relationService.getRelationList(req.user);
+    }
+}
