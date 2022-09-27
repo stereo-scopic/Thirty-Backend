48b1718ed8e9c9142eaecb5b64ed059c1d54fda3
shunnyjang <suuuu101@naver.com>
2022-09-27 14:36:54 +0900

    fix: filename unmatched bug

--- src/utils/file-upload.ts
+++ src/utils/file-upload.ts
@@ -9,15 +9,16 @@ export async function uploadFileOnAwsS3Bucket(file: any, directory: string) {
     },
   });
   try {
-    console.log('file', file);
+    const now = Date.now()
     const upload = await new AWS.S3()
       .putObject({
-        Key: `${directory}/${Date.now() + file.originalname}`,
+        ACL: 'public-read',
+        Key: `${directory}/${now + file.originalname}`,
         Body: file.buffer,
         Bucket: process.env.AWS_S3_BUCKET_NAME,
       })
       .promise();
-    return `${process.env.AWS_S3_BUCKET_URL}/${directory}/${file.originalname}`;
+    return `${process.env.AWS_S3_BUCKET_URL}/${directory}/${now + file.originalname}`;
   } catch (error) {
     // throw new BadRequestException(error);
     console.log(error);
