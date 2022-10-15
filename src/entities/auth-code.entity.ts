20445dcf7b21db4f57fda3a6358258f6d0595d39
shunnyjang <suuuu101@naver.com>
2022-10-15 21:23:04 +0900

    fix: wrong property name

--- src/entities/auth-code.entity.ts
+++ src/entities/auth-code.entity.ts
@@ -1,21 +1,21 @@
-import { Entity, PrimaryKey, Property } from "@mikro-orm/core";
+import { Entity, PrimaryKey, Property } from '@mikro-orm/core';
 
 @Entity()
 export class AuthCode {
-    @PrimaryKey()
-    @Property()
-    email: string;
+  @PrimaryKey()
+  @Property()
+  email: string;
 
-    @Property({ nullable: false })
-    code: number;
+  @Property({ nullable: false })
+  code: number;
 
-    @Property({ onCreate: () => new Date() })
-    created_at: Date;
+  @Property({ onCreate: () => new Date() })
+  created_at: Date;
 
-    constructor(email: string) {
-        this.email = email;
+  constructor(email: string) {
+    this.email = email;
 
-        const [min, max] = [100001, 999999];
-        this.code = Math.floor(Math.random() * (max - min + 1)) + min;
-    }
+    const [min, max] = [100001, 999999];
+    this.code = Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
