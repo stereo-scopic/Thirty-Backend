66373ed98fb298318986454802dc45e1fc075671
shunnyjang <suuuu101@naver.com>
2022-11-15 16:53:53 +0900

    feat: add target user data

--- src/entities/blocked.entity.ts
+++ src/entities/blocked.entity.ts
@@ -1,11 +1,27 @@
-import { Entity, Property } from "@mikro-orm/core";
+import { Entity, ManyToOne, Property } from "@mikro-orm/core";
 import { BaseEntity } from "./BaseEntity";
+import { User } from "./user.entity";
 
 @Entity()
 export class Blocked extends BaseEntity{
     @Property()
     userId: string;
 
-    @Property()
+    @Property({ hidden: true })
     targetUserId: string;
+
+    @ManyToOne({
+        entity: () => User,
+        joinColumn: `target_user_id`,
+        referenceColumnName: `id`,
+        serializer: (value) => {
+            return {
+                id: value.id,
+                nickname: value.nickname,
+            }
+        },
+        persist: false,
+        eager: true,
+    })
+    targetUser: User;
 }
